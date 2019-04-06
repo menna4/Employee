f722281dd65d1ad3802d1b73c792a87bb1127e44
Menna_Ahmed <menna4a.helmi@gmail.com>
2019-04-06 16:10:50 +0200

    Project Structure created

---
+++ Employee/src/main/java/com/mycompany/employee/AppLauncher.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.employee;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Menna Helmy
+ */
+public class AppLauncher extends Application{
+    
+    
+    
+    public static void main(String args[]){}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    
+}
