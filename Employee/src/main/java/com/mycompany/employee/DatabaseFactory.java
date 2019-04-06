bdc5a5628d3d7af241d3ad658ec67086bd720788
Menna_Ahmed <menna4a.helmi@gmail.com>
2019-04-06 16:49:46 +0200

    Database Factory reading properties file and returning the database datasource is ready

--- Employee/src/main/java/com/mycompany/employee/DatabaseFactory.java
+++ Employee/src/main/java/com/mycompany/employee/DatabaseFactory.java
@@ -5,10 +5,34 @@
  */
 package com.mycompany.employee;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.sql.DataSource;
+
 /**
  *
  * @author Menna Helmy
  */
 public class DatabaseFactory {
+    public static DataSource getMySQLDataSource() throws IOException {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mysqlDS;
+
+    } // End of Method
     
 }
