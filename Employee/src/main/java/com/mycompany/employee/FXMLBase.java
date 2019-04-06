89fc2d3423bf6e0a071a35eebcc0e6484a349631
Arafa <abdoarafa72@yahoo.com>
2019-04-06 17:16:34 +0200

    Main Functionality

--- Employee/src/main/java/com/mycompany/employee/FXMLBase.java
+++ Employee/src/main/java/com/mycompany/employee/FXMLBase.java
@@ -1,7 +1,9 @@
 package com.mycompany.employee;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,7 +20,6 @@ public class FXMLBase extends AnchorPane {
     ResultSet resultSet;
     boolean flag = false;
     boolean lastRow=false;
-    
     protected final Pane pane;
     protected final Label label;
     protected final Label label0;
@@ -26,19 +27,21 @@ public class FXMLBase extends AnchorPane {
     protected final Label label2;
     protected final Label label3;
     protected final Label label4;
-    protected final TextField textField;
-    protected final TextField textField0;
-    protected final TextField textField1;
-    protected final TextField textField2;
-    protected final TextField textField3;
-    protected final TextField textField4;
-    protected final Button button;
-    protected final Button button0;
-    protected final Button button1;
-    protected final Button button2;
-    protected final Button button3;
-    protected final Button button4;
-    protected final Button button5;
+
+    protected final TextField txt_id;
+    protected final TextField txt_fname;
+    protected final TextField txt_mname;
+    protected final TextField txt_lname;
+    protected final TextField txt_email;
+    protected final TextField txt_phone;
+
+    protected final Button btn_new;
+    protected final Button btn_update;
+    protected final Button btn_delete;
+    protected final Button btn_first;
+    protected final Button btn_previous;
+    protected final Button btn_next;
+    protected final Button btn_last;
 
     public FXMLBase() {
 
@@ -49,19 +52,19 @@ public class FXMLBase extends AnchorPane {
         label2 = new Label();
         label3 = new Label();
         label4 = new Label();
-        textField = new TextField();
-        textField0 = new TextField();
-        textField1 = new TextField();
-        textField2 = new TextField();
-        textField3 = new TextField();
-        textField4 = new TextField();
-        button = new Button();
-        button0 = new Button();
-        button1 = new Button();
-        button2 = new Button();
-        button3 = new Button();
-        button4 = new Button();
-        button5 = new Button();
+        txt_id = new TextField();
+        txt_fname = new TextField();
+        txt_mname = new TextField();
+        txt_lname = new TextField();
+        txt_email = new TextField();
+        txt_phone = new TextField();
+        btn_new = new Button();
+        btn_update = new Button();
+        btn_delete = new Button();
+        btn_first = new Button();
+        btn_previous = new Button();
+        btn_next = new Button();
+        btn_last = new Button();
 
         setId("AnchorPane");
         setPrefHeight(472.0);
@@ -106,70 +109,70 @@ public class FXMLBase extends AnchorPane {
         label4.setPrefWidth(86.0);
         label4.setText("Phone");
 
-        textField.setLayoutX(118.0);
-        textField.setLayoutY(55.0);
+        txt_id.setLayoutX(118.0);
+        txt_id.setLayoutY(55.0);
 
-        textField0.setLayoutX(118.0);
-        textField0.setLayoutY(101.0);
+        txt_fname.setLayoutX(118.0);
+        txt_fname.setLayoutY(101.0);
 
-        textField1.setLayoutX(118.0);
-        textField1.setLayoutY(153.0);
+        txt_mname.setLayoutX(118.0);
+        txt_mname.setLayoutY(153.0);
 
-        textField2.setLayoutX(118.0);
-        textField2.setLayoutY(208.0);
+        txt_lname.setLayoutX(118.0);
+        txt_lname.setLayoutY(208.0);
 
-        textField3.setLayoutX(118.0);
-        textField3.setLayoutY(275.0);
+        txt_email.setLayoutX(118.0);
+        txt_email.setLayoutY(275.0);
 
-        textField4.setLayoutX(118.0);
-        textField4.setLayoutY(333.0);
+        txt_phone.setLayoutX(118.0);
+        txt_phone.setLayoutY(333.0);
 
-        button.setLayoutX(118.0);
-        button.setLayoutY(411.0);
-        button.setMnemonicParsing(false);
-        button.setPrefHeight(25.0);
-        button.setPrefWidth(50.0);
-        button.setText("New");
+        btn_new.setLayoutX(118.0);
+        btn_new.setLayoutY(411.0);
+        btn_new.setMnemonicParsing(false);
+        btn_new.setPrefHeight(25.0);
+        btn_new.setPrefWidth(50.0);
+        btn_new.setText("New");
 
-        button0.setLayoutX(184.0);
-        button0.setLayoutY(411.0);
-        button0.setMnemonicParsing(false);
-        button0.setPrefHeight(25.0);
-        button0.setPrefWidth(61.0);
-        button0.setText("Update");
+        btn_update.setLayoutX(184.0);
+        btn_update.setLayoutY(411.0);
+        btn_update.setMnemonicParsing(false);
+        btn_update.setPrefHeight(25.0);
+        btn_update.setPrefWidth(61.0);
+        btn_update.setText("Update");
 
-        button1.setLayoutX(263.0);
-        button1.setLayoutY(411.0);
-        button1.setMnemonicParsing(false);
-        button1.setPrefHeight(25.0);
-        button1.setPrefWidth(61.0);
-        button1.setText("Delete");
+        btn_delete.setLayoutX(263.0);
+        btn_delete.setLayoutY(411.0);
+        btn_delete.setMnemonicParsing(false);
+        btn_delete.setPrefHeight(25.0);
+        btn_delete.setPrefWidth(61.0);
+        btn_delete.setText("Delete");
 
-        button2.setLayoutX(344.0);
-        button2.setLayoutY(411.0);
-        button2.setMnemonicParsing(false);
-        button2.setPrefHeight(25.0);
-        button2.setPrefWidth(61.0);
-        button2.setText("First");
+        btn_first.setLayoutX(344.0);
+        btn_first.setLayoutY(411.0);
+        btn_first.setMnemonicParsing(false);
+        btn_first.setPrefHeight(25.0);
+        btn_first.setPrefWidth(61.0);
+        btn_first.setText("First");
 
-        button3.setLayoutX(419.0);
-        button3.setLayoutY(411.0);
-        button3.setMnemonicParsing(false);
-        button3.setText("Pervious");
+        btn_previous.setLayoutX(419.0);
+        btn_previous.setLayoutY(411.0);
+        btn_previous.setMnemonicParsing(false);
+        btn_previous.setText("Pervious");
 
-        button4.setLayoutX(497.0);
-        button4.setLayoutY(411.0);
-        button4.setMnemonicParsing(false);
-        button4.setPrefHeight(25.0);
-        button4.setPrefWidth(50.0);
-        button4.setText("Next");
+        btn_next.setLayoutX(497.0);
+        btn_next.setLayoutY(411.0);
+        btn_next.setMnemonicParsing(false);
+        btn_next.setPrefHeight(25.0);
+        btn_next.setPrefWidth(50.0);
+        btn_next.setText("Next");
 
-        button5.setLayoutX(566.0);
-        button5.setLayoutY(411.0);
-        button5.setMnemonicParsing(false);
-        button5.setPrefHeight(25.0);
-        button5.setPrefWidth(50.0);
-        button5.setText("Last");
+        btn_last.setLayoutX(566.0);
+        btn_last.setLayoutY(411.0);
+        btn_last.setMnemonicParsing(false);
+        btn_last.setPrefHeight(25.0);
+        btn_last.setPrefWidth(50.0);
+        btn_last.setText("Last");
 
         pane.getChildren().add(label);
         pane.getChildren().add(label0);
@@ -177,20 +180,103 @@ public class FXMLBase extends AnchorPane {
         pane.getChildren().add(label2);
         pane.getChildren().add(label3);
         pane.getChildren().add(label4);
-        pane.getChildren().add(textField);
-        pane.getChildren().add(textField0);
-        pane.getChildren().add(textField1);
-        pane.getChildren().add(textField2);
-        pane.getChildren().add(textField3);
-        pane.getChildren().add(textField4);
-        pane.getChildren().add(button);
-        pane.getChildren().add(button0);
-        pane.getChildren().add(button1);
-        pane.getChildren().add(button2);
-        pane.getChildren().add(button3);
-        pane.getChildren().add(button4);
-        pane.getChildren().add(button5);
+        pane.getChildren().add(txt_id);
+        pane.getChildren().add(txt_fname);
+        pane.getChildren().add(txt_mname);
+        pane.getChildren().add(txt_lname);
+        pane.getChildren().add(txt_email);
+        pane.getChildren().add(txt_phone);
+        pane.getChildren().add(btn_new);
+        pane.getChildren().add(btn_update);
+        pane.getChildren().add(btn_delete);
+        pane.getChildren().add(btn_first);
+        pane.getChildren().add(btn_previous);
+        pane.getChildren().add(btn_next);
+        pane.getChildren().add(btn_last);
         getChildren().add(pane);
+        ////////////////////////////////////////////////////////////////////////////////
 
+        try {
+
+            dataSource = DatabaseFactory.getMySQLDataSource();
+            connection = dataSource.getConnection();
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            resultSet = statement.executeQuery("select * from clerk");
+            resultSet.next();
+            if(resultSet.wasNull())
+            {
+                
+            }
+            else
+            {
+            txt_id.setText(resultSet.getInt("id") + "");
+            txt_fname.setText(resultSet.getString("fname") + "");
+            txt_mname.setText(resultSet.getString("mname") + "");
+            txt_lname.setText(resultSet.getString("lname") + "");
+            txt_email.setText(resultSet.getString("email") + "");
+            txt_phone.setText(resultSet.getInt("phone") + "");  
+            }
+            
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        btn_next.setOnAction((event) -> {
+            getNext();
+        });
+        btn_previous.setOnAction((event) -> {
+            getPrevious();
+        });
+        btn_first.setOnAction((event) -> {
+            getFirst();
+        });
+        btn_last.setOnAction((event) -> {
+            getLast();
+        });
+        btn_new.setOnAction((event) -> {
+            addNewPerson();
+        });
+        btn_update.setOnAction((event) -> {
+            update();
+        });
+        btn_delete.setOnAction((event) -> {
+            delete();
+        });
+    }
+
+    private void getNext() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void getPrevious() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void getFirst() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void getLast() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void addNewPerson() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void update() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void delete() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
